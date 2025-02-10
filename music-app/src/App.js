85b9ad5128f1cc2210d3ed8a378f0e818b605252
dbh2054fly <dbh2054@stern.nyu.edu>
2025-02-10 00:18:05 -0500

    students page and components

--- music-app/src/App.js
+++ music-app/src/App.js
@@ -1,24 +1,22 @@
 import logo from './logo.svg';
 import './App.css';
+import StudentsPage from './pages/StudentsPage';
+import { BrowserRouter as Router, Route, Routes } from 'react-router-dom';
+import Sidebar from './components/Sidebar';
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <Router>
+      <div className="app-layout">
+        <Sidebar />
+        <div className="main-content">
+          <Routes>
+            <Route path="/students" element={<StudentsPage />} />
+
+          </Routes>
+        </div>
+      </div>
+    </Router>
   );
 }
 
---
+++ music-app/src/components/StudentTable.js
@@ -0,0 +1,56 @@
+import React from 'react';
+import StudentRow from './StudentRow';
+
+const students = [
+    {
+      id: 1,
+      studentName: 'Dustin',
+      parentName: 'Wen',
+      parentEmail: 'Dustin.he@gmail.com',
+      parentPhone: '614-923-3076',
+      lessonTime: '30 mins',
+      availability: {
+        Monday: '1:00pm-3:00pm',
+        Wednesday: '1:00pm-3:00pm',
+        Friday: '1:00pm-3:00pm',
+      }
+    },
+    {
+      id: 2,
+      studentName: 'Dustin',
+      parentName: 'Maggie',
+      parentEmail: 'Dustin.he@gmail.com',
+      parentPhone: '614-923-3076',
+      lessonTime: '1 hr',
+      availability: {
+        Monday: '1:00pm-3:00pm',
+        Wednesday: '1:00pm-3:00pm',
+        Friday: '1:00pm-3:00pm',
+      }
+    },
+    // Add more dummy students as needed
+  ];
+
+function StudentTable({ }) {
+    return (
+        <div className="table-container">
+            <table>
+            <thead>
+                <tr>
+                    <th>StudentName</th>
+                    <th>Parent Name</th>
+                    <th>Parent Email</th>
+                    <th>Parent Phone</th>
+                    <th>Lesson Time</th>
+                    <th>Availability</th>
+                </tr>
+            </thead>
+            <tbody>
+                {students.map(student => <StudentRow key={student.id} student={student} />)}
+            </tbody>
+        </table>
+        </div>
+    );
+}
+
+export default StudentTable;
