070430a4e627ef71aebd37054a95044a7fad8a07
dbh2054fly <dbh2054@stern.nyu.edu>
2025-02-13 11:11:02 -0500

    deleted random lib

--- src/lib/utils.ts
+++
@@ -1,7 +0,0 @@
-// src/lib/utils.ts
-
-// Temporary stub function
-export function cn(...classes: any[]): string {
-    // You can later implement this function as needed.
-    return classes.join(" ");
-  }
